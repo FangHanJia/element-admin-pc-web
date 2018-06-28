30a87b7aefceebd2c7228ef39fa3b2f8a8582f3d
fanghanjia <fang_hanjia@163.com>
2018-06-28 18:37:38 +0800

    编写菜单组件

--- src/components/menus/menus.vue
+++ src/components/menus/menus.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="fhj_menu">
-        <el-col >
-            <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose">
+        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+            <el-radio-button :label="false" @click.native="switchNav">展开</el-radio-button>
+            <el-radio-button :label="true">收起</el-radio-button>
+        </el-radio-group>
+        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" background-color="#324157" text-color="#fff" active-text-color="#ffd04b">
             <el-submenu index="1">
                 <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span slot="title">导航一</span>
                 </template>
                 <el-menu-item-group>
-                <template slot="title">分组一</template>
+                <span slot="title">分组一</span>
                 <el-menu-item index="1-1">选项1</el-menu-item>
                 <el-menu-item index="1-2">选项2</el-menu-item>
                 </el-menu-item-group>
@@ -20,7 +19,7 @@
                 <el-menu-item index="1-3">选项3</el-menu-item>
                 </el-menu-item-group>
                 <el-submenu index="1-4">
-                <template slot="title">选项4</template>
+                <span slot="title">选项4</span>
                 <el-menu-item index="1-4-1">选项1</el-menu-item>
                 </el-submenu>
             </el-submenu>
@@ -28,7 +27,7 @@
                 <i class="el-icon-menu"></i>
                 <span slot="title">导航二</span>
             </el-menu-item>
-            <el-menu-item index="3" disabled>
+            <el-menu-item index="3">
                 <i class="el-icon-document"></i>
                 <span slot="title">导航三</span>
             </el-menu-item>
@@ -36,14 +35,25 @@
                 <i class="el-icon-setting"></i>
                 <span slot="title">导航四</span>
             </el-menu-item>
-            </el-menu>
-        </el-col>
+        </el-menu>
     </div>
 </template>
 <script>
 import './menus.scss';
     export default {
-        
+        data() {
+            return {
+                isCollapse: true
+            };
+        },
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
     }
 </script>
 
