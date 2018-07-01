7b294ef6237938ec1b058dc793178795c082d8a7
方汉佳 <1690101402@qq.com>
2018-07-01 10:03:09 +0800

    解决冲突

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,19 +4,6 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const store = new Vuex.store({
-    state:{
-        count:0
-    },
-    mutations:{
-        increment(_state){
-            _state.count++;
-        }
-    }
-})
-
-Vue.use(Vuex);
-
 // 设置属性:state
 const state = {
     count : 0,
