2e2c4f7549859626df94c1bdec5f4836c221a06f
fanghanjia <fang_hanjia@163.com>
2018-07-10 21:23:46 +0800

    -m首页

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -6,7 +6,13 @@
 <script>
     import './dashboard.scss';
     export default {
-        
+        created(){
+            fetch('https://api.myjson.com/bins/74163')
+            .then(res =>res.json())
+            .then(json=>{
+                console.log(json.products);
+            })
+        }
     }
 </script>
 
